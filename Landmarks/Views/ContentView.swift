0b9e4e8048832fa5a179f091b7406c97468d6b95
leeyongjun604 <daniel_604@naver.com>
2023-04-03 20:42:58 +0900

    conflict test - main

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         LandmarkList()
-    }//git 실험중입니다
+    }//git 실험중입니다 라고 할뻔
 }
 
 struct ContentView_Previews: PreviewProvider {
